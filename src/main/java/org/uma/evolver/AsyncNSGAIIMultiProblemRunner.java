d52cf8bde097d5e37c3df7b258f427c666385481
ajnebro <ajnebro@users.noreply.github.com>
2023-06-08 18:07:01 +0200

    Add class AsyncNSGAIIMultiProblemRunner

--- src/main/java/org/uma/evolver/AsyncNSGAIIMultiProblemRunner.java
+++ src/main/java/org/uma/evolver/AsyncNSGAIIMultiProblemRunner.java
@@ -5,17 +5,18 @@ import java.util.List;
 import org.uma.evolver.algorithm.ConfigurableAlgorithmBuilder;
 import org.uma.evolver.algorithm.impl.ConfigurableNSGAII;
 import org.uma.evolver.problem.ConfigurableAlgorithmMultiProblem;
+import org.uma.evolver.problem.ConfigurableAlgorithmProblem;
 import org.uma.evolver.util.EvaluationObserver;
 import org.uma.evolver.util.OutputResultsManagement;
 import org.uma.evolver.util.OutputResultsManagement.OutputResultsManagementParameters;
 import org.uma.evolver.util.WriteExecutionDataToFilesObserver;
-import org.uma.jmetal.component.algorithm.EvolutionaryAlgorithm;
-import org.uma.jmetal.component.algorithm.multiobjective.NSGAIIBuilder;
-import org.uma.jmetal.component.catalogue.common.evaluation.impl.MultiThreadedEvaluation;
-import org.uma.jmetal.component.catalogue.common.termination.Termination;
+import org.uma.jmetal.auto.autoconfigurablealgorithm.AutoNSGAII;
 import org.uma.jmetal.component.catalogue.common.termination.impl.TerminationByEvaluations;
+import org.uma.jmetal.operator.crossover.CrossoverOperator;
 import org.uma.jmetal.operator.crossover.impl.SBXCrossover;
+import org.uma.jmetal.operator.mutation.MutationOperator;
 import org.uma.jmetal.operator.mutation.impl.PolynomialMutation;
+import org.uma.jmetal.parallel.asynchronous.algorithm.impl.AsynchronousMultiThreadedNSGAII;
 import org.uma.jmetal.problem.doubleproblem.DoubleProblem;
 import org.uma.jmetal.problem.multiobjective.zdt.ZDT1;
 import org.uma.jmetal.problem.multiobjective.zdt.ZDT2;
@@ -25,17 +26,22 @@ import org.uma.jmetal.problem.multiobjective.zdt.ZDT6;
 import org.uma.jmetal.qualityindicator.impl.Epsilon;
 import org.uma.jmetal.qualityindicator.impl.NormalizedHypervolume;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
-import org.uma.jmetal.util.JMetalLogger;
-import org.uma.jmetal.util.observer.impl.FrontPlotObserver;
 
 /**
- * Class configuring NSGA-II using arguments in the form <key, value> and the
+ * Class configuring NSGA-II using arguments in the form <key, value> and the {@link AutoNSGAII}
+ * class.
  *
  * @author Antonio J. Nebro (ajnebro@uma.es)
  */
-public class MetaNSGAIIForNSGAIIMultiProblemRunnerV2 {
+public class AsyncNSGAIIMultiProblemRunner {
 
   public static void main(String[] args) throws IOException {
+    CrossoverOperator<DoubleSolution> crossover;
+    MutationOperator<DoubleSolution> mutation;
+
+    int populationSize = 50;
+    int maxEvaluations = 3000;
+    int numberOfCores = 128;
 
     var indicators = List.of(new Epsilon(), new NormalizedHypervolume());
     DoubleProblem problemWhoseConfigurationIsSearchedFor = new ZDT1();
@@ -49,55 +55,50 @@ public class MetaNSGAIIForNSGAIIMultiProblemRunnerV2 {
             "resources/referenceFronts/ZDT4.csv",
             "resources/referenceFronts/ZDT6.csv"),
         indicators,
-        List.of(8000, 8000, 8000, 8000, 8000), 1);
+        List.of(8000, 8000, 8000, 8000, 8000), 3);
 
     double crossoverProbability = 0.9;
     double crossoverDistributionIndex = 20.0;
-    var crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
+    crossover = new SBXCrossover(crossoverProbability, crossoverDistributionIndex);
 
     double mutationProbability = 1.0 / configurableProblem.numberOfVariables();
     double mutationDistributionIndex = 20.0;
-    var mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
-
-    int populationSize = 50;
-    int offspringPopulationSize = 50;
-
-    int maxEvaluations = 3000 ;
-    Termination termination = new TerminationByEvaluations(maxEvaluations);
-
-    EvolutionaryAlgorithm<DoubleSolution> nsgaii = new NSGAIIBuilder<>(
-        configurableProblem,
-        populationSize,
-        offspringPopulationSize,
-        crossover,
-        mutation)
-        .setTermination(termination)
-        .setEvaluation(new MultiThreadedEvaluation<>(8, configurableProblem))
-        .build();
-
-    var evaluationObserver = new EvaluationObserver(10);
-    var frontChartObserver =
-        new FrontPlotObserver<DoubleSolution>("NSGA-II", indicators.get(0).name(), indicators.get(1).name(), problemWhoseConfigurationIsSearchedFor.name(), 50);
+    mutation = new PolynomialMutation(mutationProbability, mutationDistributionIndex);
 
     OutputResultsManagementParameters outputResultsManagementParameters = new OutputResultsManagementParameters(
         "NSGA-II", configurableProblem, problemWhoseConfigurationIsSearchedFor, indicators,
-        "outputFiles");
+        "asyncOutputFilesZDT");
     var outputResultsManagement = new OutputResultsManagement(outputResultsManagementParameters);
 
+    long initTime = System.currentTimeMillis();
+
+    AsynchronousMultiThreadedNSGAII<DoubleSolution> nsgaii =
+        new AsynchronousMultiThreadedNSGAII<>(
+            numberOfCores, configurableProblem, populationSize, crossover, mutation,
+            new TerminationByEvaluations(maxEvaluations));
+
+    EvaluationObserver evaluationObserver = new EvaluationObserver(10);
+
+    /*
+    RunTimeChartObserver<DoubleSolution> runTimeChartObserver =
+        new RunTimeChartObserver<>(
+            "NSGA-II",
+            80, 100, null, indicators.get(0).name(), indicators.get(1).name());
+    */
     var writeExecutionDataToFilesObserver = new WriteExecutionDataToFilesObserver(
         List.of(500, 1000, 1500, 2000, 2500), outputResultsManagement);
 
-    nsgaii.observable().register(evaluationObserver);
-    nsgaii.observable().register(frontChartObserver);
-    nsgaii.observable().register(writeExecutionDataToFilesObserver);
+    nsgaii.getObservable().register(evaluationObserver);
+    //nsgaii.getObservable().register(runTimeChartObserver);
+    nsgaii.getObservable().register(writeExecutionDataToFilesObserver);
 
     nsgaii.run();
 
-    JMetalLogger.logger.info(() -> "Total computing time: " + nsgaii.totalComputingTime());
+    long endTime = System.currentTimeMillis();
+
+    System.out.println("Total computing time: " + (endTime - initTime)) ;
 
     outputResultsManagement.updateSuffix("." + maxEvaluations + ".csv");
-    outputResultsManagement.writeResultsToFiles(nsgaii.result());
-
-    //System.exit(0) ;
+    outputResultsManagement.writeResultsToFiles(nsgaii.getResult());
   }
 }
