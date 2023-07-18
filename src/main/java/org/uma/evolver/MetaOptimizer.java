00f56f40ea3b56b82bb95a113152a586f332db9d
jfaldanam <me@jfaldanam.xyz>
2023-07-18 15:46:15 +0200

    Improve documentation

--- src/main/java/org/uma/evolver/MetaOptimizer.java
+++ src/main/java/org/uma/evolver/MetaOptimizer.java
@@ -6,6 +6,11 @@ import org.uma.jmetal.util.observable.Observable;
 import java.util.List;
 import java.util.Map;
 
+/*
+ * Interface for all meta-optimizers
+ * This is required as each MetaOptimizer from jMetal may have different methods,
+ * but this is the minimum required for them to work on Evolver
+*/
 public abstract class MetaOptimizer {
     public abstract void run();
     public abstract Observable<Map<String, Object>> observable();
