c2c68ff90d8412c76f34ae3570c834ea6bdf26e6
jfaldanam <me@jfaldanam.xyz>
2023-07-18 15:48:34 +0200

    Fix format

--- src/main/java/org/uma/evolver/MetaOptimizer.java
+++ src/main/java/org/uma/evolver/MetaOptimizer.java
@@ -10,10 +10,13 @@ import java.util.Map;
  * Interface for all meta-optimizers
  * This is required as each MetaOptimizer from jMetal may have different methods,
  * but this is the minimum required for them to work on Evolver
-*/
+ */
 public abstract class MetaOptimizer {
     public abstract void run();
+
     public abstract Observable<Map<String, Object>> observable();
+
     public abstract long totalComputingTime();
+
     public abstract List<DoubleSolution> result();
 }
