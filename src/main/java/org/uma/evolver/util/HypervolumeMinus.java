3659b5acdbd6fed3dace17f704be630ef39bb9a1
ajnebro <ajnebro@users.noreply.github.com>
2024-02-07 10:37:49 +0100

    Add quality indicator

---
+++ src/main/java/org/uma/evolver/util/HypervolumeMinus.java
@@ -0,0 +1,24 @@
+package org.uma.evolver.util;
+
+import org.uma.jmetal.qualityindicator.QualityIndicator;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.Hypervolume;
+import org.uma.jmetal.qualityindicator.impl.hypervolume.impl.PISAHypervolume;
+
+/**
+ * Class providing an implementation of the normalized hypervolume, which is calculated as follows:
+ * relative hypervolume = 1 - (HV of the front / HV of the reference front).
+ * Before computing this indicator it must be checked that the HV of the reference front is not zero.
+ *
+ * @author Antonio J. Nebro <antonio@lcc.uma.es>
+ */
+public class HypervolumeMinus extends PISAHypervolume {
+  @Override
+  public double compute(double[][] front) {
+    return super.compute(front) * - 1 ;
+  }
+
+  @Override
+  public String name() {
+    return "HV-" ;
+  }
+}
