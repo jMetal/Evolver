179a6e12b3173258cadd9e92e8d0f90e59e17f1b
ajnebro <ajnebro@users.noreply.github.com>
2025-09-04 10:33:08 +0200

    Add class RE3DProblemFamilyInfo

--- src/main/java/org/uma/evolver/util/problemfamilyinfo/RE3DProblemFamilyInfo.java
+++ src/main/java/org/uma/evolver/util/problemfamilyinfo/RE3DProblemFamilyInfo.java
@@ -5,19 +5,18 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.IntStream;
 import org.uma.jmetal.problem.Problem;
-import org.uma.jmetal.problem.multiobjective.dtlz.*;
+import org.uma.jmetal.problem.multiobjective.re.*;
 import org.uma.jmetal.solution.doublesolution.DoubleSolution;
 
-public class DTLZ3DProblemFamilyInfo implements ProblemFamilyInfo<DoubleSolution> {
-  private static final int DEFAULT_NUMBER_OF_EVALUATIONS = 16000;
+public class RE3DProblemFamilyInfo implements ProblemFamilyInfo<DoubleSolution> {
+  private static final int DEFAULT_NUMBER_OF_EVALUATIONS = 8000;
 
   private static final List<Problem<DoubleSolution>> problemList =
-      List.of(
-          new DTLZ1(), new DTLZ2(), new DTLZ3(), new DTLZ4(), new DTLZ5(), new DTLZ6(), new DTLZ7());
+      List.of(new RE31(), new RE32(), new RE33(), new RE34(), new RE35(), new RE36(), new RE37());
 
   private static final List<String> referenceFrontFileName =
       IntStream.range(1, problemList.size() + 1)
-          .mapToObj(id -> "resources/referenceFronts/DTLZ" + id + ".3D.csv")
+          .mapToObj(id -> "resources/referenceFronts/RE3" + id + ".csv")
           .toList();
 
   private static final List<Integer> evaluationsToOptimize =
@@ -40,6 +39,6 @@ public class DTLZ3DProblemFamilyInfo implements ProblemFamilyInfo<DoubleSolution
 
   @Override
   public String name() {
-    return "DTLZ3D";
+    return "RE3D";
   }
 }
