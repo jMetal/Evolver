586e59fc22f40a10df3df77648e6998ab059c771
ajnebro <ajnebro@users.noreply.github.com>
2025-12-19 09:27:56 +0100

    Implement Parallel Random Search with Observability for Meta-Optimization

---
+++ src/main/java/org/uma/evolver/algorithm/meta/MetaRandomSearchBuilder.java
@@ -0,0 +1,34 @@
+package org.uma.evolver.algorithm.meta;
+
+import org.uma.jmetal.problem.Problem;
+import org.uma.jmetal.solution.Solution;
+import org.uma.jmetal.util.errorchecking.Check;
+
+/**
+ * Builder for {@link RandomSearch} algorithm.
+ */
+public class MetaRandomSearchBuilder<S extends Solution<?>> {
+    private final Problem<S> problem;
+    private int maxEvaluations = 25000;
+    private int numberOfCores = 1;
+
+    public MetaRandomSearchBuilder(Problem<S> problem) {
+        this.problem = problem;
+    }
+
+    public MetaRandomSearchBuilder<S> setMaxEvaluations(int maxEvaluations) {
+        Check.valueIsNotNegative(maxEvaluations);
+        this.maxEvaluations = maxEvaluations;
+        return this;
+    }
+
+    public MetaRandomSearchBuilder<S> setNumberOfCores(int numberOfCores) {
+        Check.valueIsNotNegative(numberOfCores);
+        this.numberOfCores = numberOfCores;
+        return this;
+    }
+
+    public RandomSearch<S> build() {
+        return new RandomSearch<>(problem, maxEvaluations, numberOfCores);
+    }
+}
