ef9b4c1760630e204f967374098b214d55e53232
ajnebro <ajnebro@users.noreply.github.com>
2026-07-31 10:28:53 +0200

    test: update expected parameter count for NSGAIIDouble (33->34)
    Stale after this morning's merge conflict resolution touched
    NSGAIIDouble.yaml without updating the test.

--- src/test/java/org/uma/evolver/algorithm/nsgaii/NSGAIIDoubleTest.java
+++ src/test/java/org/uma/evolver/algorithm/nsgaii/NSGAIIDoubleTest.java
@@ -30,7 +30,7 @@ class NSGAIIDoubleTest {
         @Test
         @DisplayName("The total number of parameters is correct")
         void whenCreatingAnInstanceTheTotalNumberOfParametersIsCorrect() {
-            int numberOfFlattenedParameters = 33;
+            int numberOfFlattenedParameters = 34;
             assertEquals(
                     numberOfFlattenedParameters,
                     nsgaIIDouble.parameterSpace().parameters().size());
